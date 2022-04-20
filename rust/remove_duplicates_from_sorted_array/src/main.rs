4781e324159490519b8a07600fca5fa62b208c50
nightshadedude <nightshadedude@gmail.com>
2022-04-20 00:02:14 -0600

    remove dupes from sorted array completed

--- rust/remove_duplicates_from_sorted_array/src/main.rs
+++ rust/remove_duplicates_from_sorted_array/src/main.rs
@@ -1,7 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let mut test: Vec<i32> = vec![1,1,2];
+    println!("{}",remove_duplicates(&mut test));
 }
 
 pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-    
+    if nums.len() == 1 || nums.len() == 0 { return nums.len() as i32; }
+    let mut check = true;
+    let mut ii = 0;
+    while check {
+        if ii < nums.len()-1 {
+            if nums[ii] == nums[ii+1] {
+                nums.remove(ii);
+            } else {
+                ii += 1;
+            }
+        } else {
+            check  = false;
+        }
+    }
+    nums.len() as i32
 }
