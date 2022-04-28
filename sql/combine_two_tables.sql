0269e29f77de9b4d6c2ee50eabcd63ed9da59160
nightshadedude <nightshadedude@gmail.com>
2022-04-27 23:31:46 -0600

    combine two tables completed

---
+++ sql/combine_two_tables.sql
@@ -0,0 +1,9 @@
+-- https://leetcode.com/problems/combine-two-tables/
+
+select P.firstName
+    ,P.lastName
+    ,A.city
+    ,A.state
+from Person P
+left join Address A
+    on P.personId = A.personID
