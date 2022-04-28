af89b7b8cb71ff69be1880e52179879bfdf7c585
nightshadedude <nightshadedude@gmail.com>
2022-04-28 00:21:02 -0600

    employees earning more than thier managers completed

---
+++ sql/employees_earning_more_than_their_managers.sql
@@ -0,0 +1,7 @@
+--https://leetcode.com/problems/employees-earning-more-than-their-managers/
+
+select E1.name as Employee
+from Employee E1
+inner join Employee E2
+    on E1.managerId = E2.id
+where E1.salary > E2.salary
